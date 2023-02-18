c4ee8f0db3b7a5c885f3d4ca56e06fcee6c6c6a7
Ivan Bayu Pratama <121966880+ivanbayupratama@users.noreply.github.com>
2023-02-18 16:06:05 +0000

    Add files via upload

---
+++ soal1a.php
@@ -0,0 +1,23 @@
+<?php // 1a. PHP
+$benda = 'Topi';
+$bentuk = 'Bundar';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas 1</title>
+</head>
+<body>
+    <h2> 
+        <i> 
+            "<?php
+            echo "$benda saya $bentuk, $bentuk $benda saya.";    
+            ?>"
+        </i>
+    </h2>
+</body>
+</html>
---
+++ soal1c.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas 1</title>
+    <!-- CSS -->
+    <style>
+    .kotak {
+        width: 70px;
+        height:70px;
+        border-style: solid;
+        border-width: 1px; 
+        background-color: #537FE7;
+    }
+    .kotak p {
+        font-size: 20px;
+        text-align: center;
+        padding: 5px 0;
+    }
+
+    .container-2 {
+        margin-left: 72px;
+        margin-top: -143px;
+    }
+
+    .container-3 {
+        margin-left: 144px;
+        margin-top: -71.5px;
+    }
+    </style>
+</head>
+
+
+
+<body>
+    <!-- Baris 1 -->
+    <div class="container">
+        <div class="kotak">
+            <p>1</p>
+        </div>
+        <div class="kotak">
+            <p>2</p>
+        </div>
+        <div class="kotak">
+            <p>3</p>
+        </div>
+    </div>  
+        <!-- Akhir Baris 1 -->
+
+        <!-- Baris 2 -->
+    <div class="container-2">
+        <div class="kotak">
+            <p>2</p>
+        </div>
+        <div class="kotak">
+            <p>3</p>
+        </div>
+        <!-- Akhir Baris 2 -->
+    </div>
+
+    <div class="container-3">
+        <div class="kotak">
+            <p>3</p>
+        </div>
+    </div>
+</body>
+</html>
