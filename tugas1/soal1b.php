7aea9eb6ca933f8e1f40438657a2d1ac0bebd185
Ivan Bayu Pratama <121966880+ivanbayupratama@users.noreply.github.com>
2023-02-20 10:49:04 +0000

    Add files via upload

---
+++ tugas1/soal1b.php
@@ -0,0 +1,26 @@
+<?php // 1b.PHP
+$digit_npm = 57;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas 1</title>
+</head>
+<body>
+
+
+    <p>
+        Aku adalah angka <b><?php echo $digit_npm?></b><br>
+        Jika aku dikali 5, maka aku sekarang menjadi <b><?php echo $digit_npm*=5 ?></b><br>
+        Jika aku dibagi 2, maka aku sekarang menjadi <b><?php echo $digit_npm/=2 ?></b><br>
+        Jika aku ditambah 75, maka aku sekarang menjadi <b><?php echo $digit_npm+=75 ?></b><br>
+        Jika aku dikurang 20, maka aku sekarang menjadi <b><?php echo $digit_npm-=20 ?></b><br>
+    </p>
+
+
+</body>
+</html>
